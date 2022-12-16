37361ec302846c5ca105ecb486ca31f0b7d01984
RQUST <rqust@yandex.com>
2022-12-16 21:37:17 +0200

    sprint 5 attempt 1

---
+++ search-server/document.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+
+
+using namespace std::string_literals;
+
+struct Document
+{
+    Document() = default;
+    Document(int id, double relevance, int rating);
+
+    int id = 0;
+    double relevance = 0.0;
+    int rating = 0;
+};
+
+std::ostream& operator<<(std::ostream& out, const Document& document);
+
+void PrintDocument(const Document& document);
+
+enum class DocumentStatus 
+{
+    ACTUAL,
+    IRRELEVANT,
+    BANNED,
+    REMOVED,
+};
