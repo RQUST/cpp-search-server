373f1b7b9d55653532b6ba92119aacae143dd444
RQUST <rqust@yandex.com>
2022-12-28 16:03:19 +0200

    sprint 6 attempt 1

--- search-server/search_server.h
+++ search-server/search_server.h
@@ -46,11 +46,15 @@ public:
 
 	size_t GetDocumentCount() const { return documents_.size(); }
 
-	int GetDocumentId(int index) const { return document_ids_.at(index); }
+	int GetDocumentId(int index) const { return document_ids_.count(index); }
 
-	auto begin() const { return document_ids_.begin();}
+	auto begin() const {
+		return document_ids_.begin();
+	}
 
-	auto end() const { return document_ids_.end(); }
+	auto end() const {
+		return document_ids_.end();
+	}
 
 	const std::map<std::string, double>& GetWordFrequencies(int document_id) const;
 
@@ -70,7 +74,7 @@ private:
 	std::map<std::string, std::map<int, double>> word_to_document_freqs_;
 	std::map<int, std::map<std::string, double>> document_to_word_freqs_;
 	std::map<int, DocumentData> documents_;
-	std::vector<int> document_ids_;
+	std::set<int> document_ids_;
 	bool IsStopWord(const std::string& word) const {
 		return stop_words_.count(word) > 0;
 	}
